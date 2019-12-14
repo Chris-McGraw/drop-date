6417d73899610ddb8af7ba6bd364c99e82c2ccf2
Chris McGraw <chris.rmcgraw1@gmail.com>
2019-12-14 11:22:50 -0500

    add recentReleaseRow directive

--- js/controllers/GamesController.js
+++ js/controllers/GamesController.js
@@ -1,6 +1,11 @@
-app.controller("GamesController", ["$scope", "userSearch", "$location", function($scope, userSearch, $location) {
+app.controller("GamesController", ["$scope", "giantBombApi", "userSearch", "$location", function($scope, giantBombApi, userSearch, $location) {
   $scope.title = "Current View : Games";
   $scope.buttonText = "Search";
+
+  $scope.baseUrl = giantBombApi.baseUrl();
+  $scope.key = giantBombApi.key();
+  $scope.endUrl = giantBombApi.endUrl();
+  $scope.callback = giantBombApi.callback();
 
   $scope.getInput = function() {
     var input = $scope.inputVal;
