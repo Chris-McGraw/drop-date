05c3737c6acaa7fd0d2d74e9f4abf996ffdfe3b5
Chris McGraw <chris.rmcgraw1@gmail.com>
2019-12-19 14:30:23 -0500

    add tvRow directive

---
+++ js/directives/tvRow.js
@@ -0,0 +1,40 @@
+app.directive("tvRow", ["jsonPad", "tvApi", "localDate", function(jsonPad, tvApi, localDate) {
+  return {
+    restrict: "E",
+    scope: {
+      type: "@"
+    },
+    templateUrl: "js/directives/tvRow.html",
+    link: function(scope, element, attrs) {
+
+      if(scope.type === "tv") {
+        scope.rowTitle = "Television";
+
+        jsonPad.getData( tvApi.recentUrl(), tvApi.callback() ).then(
+          function successCallback(response) {
+            scope.shows = response.data.results;
+        });
+      }
+      else if(scope.type === "recent") {
+        scope.rowTitle = "Recent";
+
+        jsonPad.getData( tvApi.recentUrl(), tvApi.callback() ).then(
+          function successCallback(response) {
+            scope.shows = response.data.results;
+        });
+      }
+      // else if(scope.type === "upcoming") {
+      //   scope.rowTitle = "Upcoming";
+      //
+      //   jsonPad.getData( gameApi.upcomingUrl(), gameApi.callback() ).then(
+      //     function successCallback(response) {
+      //       scope.games = response.data.results;
+      //   });
+      // }
+      else {
+        console.log("broke it");
+      }
+
+    }
+  };
+}]);
