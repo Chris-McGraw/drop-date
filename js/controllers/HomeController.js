3f36beb9998cea7d04f2679b1a094f03cdb91701
Chris McGraw <chris.rmcgraw1@gmail.com>
2019-12-18 22:02:15 -0500

    add movieRow directive & edit movieApi service

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,13 +1,6 @@
-app.controller("HomeController", ["$scope", "jsonPad", "movieApi", "tvApi", function($scope, jsonPad, movieApi, tvApi) {
+app.controller("HomeController", ["$scope", "jsonPad", "tvApi", function($scope, jsonPad, tvApi) {
   $scope.title = "Current View : Home";
-  $scope.movieTitle = "Movies";
   $scope.tvTitle = "Television";
-
-// ----------------------- MOVIES
-  jsonPad.getData( movieApi.recentUrl(), movieApi.callback() ).then(
-    function successCallback(response) {
-      $scope.movies = response.data.results;
-  });
 
 // ------------------- TELEVISION
   jsonPad.getData( tvApi.recentUrl(), tvApi.callback() ).then(
--- js/services/movieApi.js
+++ js/services/movieApi.js
@@ -3,13 +3,19 @@ app.service("movieApi", ["localDate", "userSearch", function(localDate, userSear
 
   this.recentUrl = function() {
     return "https://api.themoviedb.org/3/discover/movie?api_key=" + key
-    + "&language=en-US&region=US&sort_by=release_date.desc&include_adult=false&include_video=false&page=1&release_date.lte="
-    + localDate.getCurrentDate();
+    + "&language=en-US&region=US&sort_by=primary_release_date.desc&include_adult=false&include_video=false&page=1"
+    + "&primary_release_date.lte=" + localDate.getCurrentDate();
+  }
+
+  this.upcomingUrl = function() {
+    return "https://api.themoviedb.org/3/discover/movie?api_key=" + key
+    + "&language=en-US&region=US&sort_by=primary_release_date.asc&include_adult=false&include_video=false&page=1"
+    + "&primary_release_date.gte=" + localDate.getTomorrowDate();
   }
 
   this.searchUrl = function() {
-    return "https://api.themoviedb.org/3/search/movie?api_key=" + key + "&query="
-    + userSearch.getData() + "&language=en-US&page=1&include_adult=false&region=US";
+    return "https://api.themoviedb.org/3/search/movie?api_key=" + key
+    + "&query=" + userSearch.getData() + "&language=en-US&page=1&include_adult=false&region=US";
   }
 
   this.callback = function() {
