1da536b95324ed835f0444d287b8ed0f489a2c99
Chris McGraw <chris.rmcgraw1@gmail.com>
2019-12-21 10:58:30 -0500

    edit gameRow directive(filter out upcoming releases without full date)

--- js/directives/gameRow.js
+++ js/directives/gameRow.js
@@ -1,4 +1,4 @@
-app.directive("gameRow", ["jsonPad", "gameApi", "localDate", function(jsonPad, gameApi, localDate) {
+app.directive("gameRow", ["jsonPad", "gameApi", "localDate", "$filter", function(jsonPad, gameApi, localDate, $filter) {
   return {
     restrict: "E",
     scope: {
@@ -6,7 +6,6 @@ app.directive("gameRow", ["jsonPad", "gameApi", "localDate", function(jsonPad, g
     },
     templateUrl: "js/directives/gameRow.html",
     link: function(scope, element, attrs) {
-
       if(scope.type === "games") {
         scope.rowTitle = "Video Games";
 
@@ -15,6 +14,7 @@ app.directive("gameRow", ["jsonPad", "gameApi", "localDate", function(jsonPad, g
             scope.games = response.data.results;
         });
       }
+
       else if(scope.type === "recent") {
         scope.rowTitle = "Recent";
 
@@ -23,16 +23,21 @@ app.directive("gameRow", ["jsonPad", "gameApi", "localDate", function(jsonPad, g
             scope.games = response.data.results;
         });
       }
+
       else if(scope.type === "upcoming") {
         scope.rowTitle = "Upcoming";
 
         jsonPad.getData( gameApi.upcomingUrl(), gameApi.callback() ).then(
           function successCallback(response) {
-            scope.games = response.data.results;
+            scope.games = $filter("filter")(response.data.results, {expected_release_year:"",
+              expected_release_month:"",
+              expected_release_day:""}
+            );
         });
       }
+
       else {
-        console.log("broke it");
+        console.log("error");
       }
 
     }
--- js/services/localDate.js
+++ js/services/localDate.js
@@ -11,19 +11,6 @@ app.service("localDate", function() {
     return currentDate;
   }
 
-// ------------ GET PREVIOUS DATE
-  var previous = new Date();
-  previous.setDate(previous.getDate() - 14);
-  var prevDay = String(previous.getDate()).padStart(2, "0");
-  var prevMonth = String(previous.getMonth() + 1).padStart(2, "0");
-  var prevYear = previous.getFullYear();
-
-  var previousDate = prevYear + "-" + prevMonth + "-" + prevDay;
-
-  this.getPreviousDate = function() {
-    return previousDate;
-  }
-
 // ------------ GET TOMORROW DATE
   var tomorrow = new Date();
   tomorrow.setDate(tomorrow.getDate() + 1);
@@ -37,9 +24,22 @@ app.service("localDate", function() {
     return tomorrowDate;
   }
 
+// ------------ GET PREVIOUS DATE
+  var previous = new Date();
+  previous.setDate(previous.getDate() - 90);
+  var prevDay = String(previous.getDate()).padStart(2, "0");
+  var prevMonth = String(previous.getMonth() + 1).padStart(2, "0");
+  var prevYear = previous.getFullYear();
+
+  var previousDate = prevYear + "-" + prevMonth + "-" + prevDay;
+
+  this.getPreviousDate = function() {
+    return previousDate;
+  }
+
 // -------------- GET FUTURE DATE
   var future = new Date();
-  future.setDate(future.getDate() + 14);
+  future.setDate(future.getDate() + 90);
   var ftrDay = String(future.getDate()).padStart(2, "0");
   var ftrMonth = String(future.getMonth() + 1).padStart(2, "0");
   var ftrYear = future.getFullYear();
