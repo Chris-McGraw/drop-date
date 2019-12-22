e17c94e21bd43229df187eb7dac067c0f81479e2
Chris McGraw <chris.rmcgraw1@gmail.com>
2019-12-21 19:45:00 -0500

    edit movieResult/Detail controllers/views & tvResult/Detail controllers/views
    add functionality to get backup images

--- js/controllers/TvDetailController.js
+++ js/controllers/TvDetailController.js
@@ -5,5 +5,12 @@ app.controller("TvDetailController", ["$scope", "jsonPad", "tvApi", "$routeParam
   jsonPad.getData( tvApi.searchUrl(), tvApi.callback() ).then(
     function successCallback(response) {
       $scope.detail = response.data.results[$routeParams.id];
+
+      if($scope.detail.poster_path === null) {
+        $scope.detail.img_path = "../../imgs/tv-backup.png";
+      }
+      else {
+        $scope.detail.img_path = "http://image.tmdb.org/t/p/w185" + $scope.detail.poster_path;
+      }
   });
 }]);
