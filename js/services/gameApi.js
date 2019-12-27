df2163b699beefee7773fc870b0ed5ffb5d597d7
Chris McGraw <chris.rmcgraw1@gmail.com>
2019-12-27 10:22:14 -0500

    edit gameRow directive(add game details link)

--- js/services/gameApi.js
+++ js/services/gameApi.js
@@ -3,14 +3,14 @@ app.service("gameApi", ["localDate", "userSearch", function(localDate, userSearc
 
   this.recentUrl = function() {
     return "https://www.giantbomb.com/api/games/?api_key=" + key
-    + "&format=jsonp&limit=100&offset=0&field_list=name,game,image,expected_release_day,expected_release_month,expected_release_year,original_release_date"
+    + "&format=jsonp&limit=100&offset=0&field_list=name,id,image,expected_release_day,expected_release_month,expected_release_year,original_release_date"
     + "&filter=original_release_date:" + localDate.getPreviousDate() + "%2000:00:00|"
     + localDate.getCurrentDate() + "%2000:00:00&sort=original_release_date:desc";
   }
 
   this.upcomingUrl = function() {
     return "https://www.giantbomb.com/api/games/?api_key=" + key
-    + "&format=jsonp&limit=100&offset=0&field_list=name,game,image,expected_release_day,expected_release_month,expected_release_year,original_release_date"
+    + "&format=jsonp&limit=100&offset=0&field_list=name,id,image,expected_release_day,expected_release_month,expected_release_year,original_release_date"
     + "&filter=original_release_date:" + localDate.getTomorrowDate() + "%2000:00:00|"
     + localDate.getFutureDate() + "%2000:00:00&sort=original_release_date:asc"
   }
