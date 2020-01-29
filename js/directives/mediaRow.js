5d4812754609095e14ca7e415d8618c6ab3933ca
Chris McGraw <chris.rmcgraw1@gmail.com>
2020-01-29 14:52:17 -0500

    add fillMediaRow service

--- js/directives/mediaRow.js
+++ js/directives/mediaRow.js
@@ -1,4 +1,4 @@
-app.directive("mediaRow", ["jsonPad", "movieApi", "tvApi", "gameApi", "localDate", "$filter", function(jsonPad, movieApi, tvApi, gameApi, localDate, $filter) {
+app.directive("mediaRow", ["fillMediaRow", function(fillMediaRow) {
   return {
     restrict: "E",
     scope: {
@@ -14,57 +14,15 @@ app.directive("mediaRow", ["jsonPad", "movieApi", "tvApi", "gameApi", "localDate
 
 // ____ ATTRIBUTE HANDLERS
       if(scope.media === "movies") {
-        function movieIdPath(movie) {
-          movie.idPath = "#/movies/detail/?id=" + movie.id;
-          // console.log(movie.idPath);
-        }
-
-        function backupMovieImages(movie) {
-          if(movie.poster_path === null) {
-            movie.imgPath = "../../imgs/movie-backup.png";
-          }
-          else {
-            movie.imgPath = "http://image.tmdb.org/t/p/w185" + movie.poster_path;
-          }
-        }
-
-      // ---
-
         if(scope.type === "recent") {
-          jsonPad.getData( movieApi.recentUrl(), movieApi.callback() ).then(
-            function successCallback(response) {
-              var lessThan = function(prop, val) {
-                return function(item) {
-                  return item[prop] <= val;
-                }
-              }
-
-              var recentMovies = $filter("filter")(response.data.results, lessThan("release_date", localDate.getCurrentDate() ));
-              scope.mediaList = $filter("orderBy")(recentMovies, "release_date", reverse = true);
-
-              angular.forEach(scope.mediaList, function(movie) {
-                movieIdPath(movie);
-                backupMovieImages(movie);
-              });
+          fillMediaRow.getRecentMovies().then(function(data) {
+            scope.mediaList = data;
           });
         }
 
         else if(scope.type === "upcoming") {
-          jsonPad.getData( movieApi.upcomingUrl(), movieApi.callback() ).then(
-            function successCallback(response) {
-              var greaterThan = function(prop, val) {
-                return function(item) {
-                  return item[prop] >= val;
-                }
-              }
-
-              var upcomingMovies = $filter("filter")(response.data.results, greaterThan("release_date", localDate.getTomorrowDate() ));
-              scope.mediaList = $filter("orderBy")(upcomingMovies, "release_date");
-
-              angular.forEach(scope.mediaList, function(movie) {
-                movieIdPath(movie);
-                backupMovieImages(movie);
-              });
+          fillMediaRow.getUpcomingMovies().then(function(data) {
+            scope.mediaList = data;
           });
         }
       }
@@ -72,61 +30,15 @@ app.directive("mediaRow", ["jsonPad", "movieApi", "tvApi", "gameApi", "localDate
 // ---
 
       else if(scope.media === "tv") {
-        function checkOrigin(prop, val) {
-          return function(item) {
-            return item[prop].length === 0 || item[prop].toString() === val;
-          }
-        }
-
-        function tvIdPath(show) {
-          show.idPath = "#/tv/detail/?id=" + show.id;
-        }
-
-        function backupTvImages(show) {
-          if(show.poster_path === null) {
-            show.imgPath = "../../imgs/tv-backup.png";
-          }
-          else {
-            show.imgPath = "http://image.tmdb.org/t/p/w185" + show.poster_path;
-          }
-        }
-
-        function tvTitle(show) {
-          show.title = show.name;
-        }
-
-        function tvRelease(show) {
-          show.release_date = show.first_air_date;
-        }
-
-      // ---
-
         if(scope.type === "recent") {
-          jsonPad.getData( tvApi.recentUrl(), tvApi.callback() ).then(
-            function successCallback(response) {
-              scope.mediaList = $filter("filter")( response.data.results, checkOrigin("origin_country", "US") );
-
-              angular.forEach(scope.mediaList, function(show) {
-                tvIdPath(show);
-                backupTvImages(show);
-                tvTitle(show);
-                tvRelease(show);
-              });
+          fillMediaRow.getRecentTv().then(function(data) {
+            scope.mediaList = data;
           });
         }
 
         else if(scope.type === "upcoming") {
-          jsonPad.getData( tvApi.upcomingUrl(), tvApi.callback() ).then(
-            function successCallback(response) {
-              var upcomingTv = $filter("filter")( response.data.results, checkOrigin("origin_country", "US") );
-              scope.mediaList = $filter("orderBy")(upcomingTv, "first_air_date");
-
-              angular.forEach(scope.mediaList, function(show) {
-                tvIdPath(show);
-                backupTvImages(show);
-                tvTitle(show);
-                tvRelease(show);
-              });
+          fillMediaRow.getUpcomingTv().then(function(data) {
+            scope.mediaList = data;
           });
         }
       }
@@ -134,86 +46,15 @@ app.directive("mediaRow", ["jsonPad", "movieApi", "tvApi", "gameApi", "localDate
 // ---
 
        else if(scope.media === "games") {
-         function getTrimmedArray(results, trimmedArray) {
-           for(i = 0; i < results.length; i++) {
-             if(i === 0) {
-               trimmedArray.push(results[i]);
-             }
-
-             else if(trimmedArray.length < 20) {
-               var hasDuplicate = false;
-
-               for(v = 0; v < trimmedArray.length; v++) {
-                 if(results[i].game.id === trimmedArray[v].game.id) {
-                   hasDuplicate = true;
-                 }
-               }
-
-               if(hasDuplicate === false) {
-                 trimmedArray.push(results[i]);
-               }
-             }
-           }
-           return trimmedArray;
-         }
-
-         function gameIdPath(game) {
-           game.idPath = "#/games/detail/?id=" + game.game.id;
-         }
-
-         function backupGameImages(game) {
-           if(game.image.small_url === "https://www.giantbomb.com/api/image/scale_small/3026329-gb_default-16_9.png") {
-             game.imgPath = "../../imgs/game-backup.png";
-           }
-           else {
-             game.imgPath = game.image.small_url;
-           }
-         }
-
-         function gameTitle(game) {
-           game.title = game.name;
-         }
-
-         function formatDate(game) {
-           game.release_date = new Date(game.expected_release_year + "/" + game.expected_release_month + "/" + game.expected_release_day);
-         }
-
-         // ---
-
          if(scope.type === "recent") {
-           jsonPad.getData( gameApi.recentUrl(), gameApi.callback() ).then(
-             function successCallback(response) {
-               var trimmedArray = [];
-               getTrimmedArray(response.data.results, trimmedArray);
-
-               scope.mediaList = $filter("filter")(trimmedArray, {release_date:""});
-
-               angular.forEach(scope.mediaList, function(game) {
-                 gameIdPath(game);
-                 backupGameImages(game);
-                 gameTitle(game);
-                 game.release_date = game.release_date.replace(/ /g,"T");
-               });
+           fillMediaRow.getRecentGames().then(function(data) {
+             scope.mediaList = data;
            });
          }
 
          else if(scope.type === "upcoming") {
-           jsonPad.getData( gameApi.upcomingUrl(), gameApi.callback() ).then(
-             function successCallback(response) {
-               var trimmedArray = [];
-               getTrimmedArray(response.data.results, trimmedArray);
-
-               scope.mediaList = $filter("filter")(trimmedArray, {expected_release_year:"",
-                 expected_release_month:"",
-                 expected_release_day:""}
-               );
-
-               angular.forEach(scope.mediaList, function(game) {
-                 gameIdPath(game);
-                 backupGameImages(game);
-                 gameTitle(game);
-                 formatDate(game);
-               });
+           fillMediaRow.getUpcomingGames().then(function(data) {
+             scope.mediaList = data;
            });
          }
        }
---
+++ js/services/fillMediaRow.js
@@ -0,0 +1,231 @@
+app.service("fillMediaRow", ["jsonPad", "movieApi", "tvApi", "gameApi", "localDate", "$filter", function(jsonPad, movieApi, tvApi, gameApi, localDate, $filter) {
+
+
+/* --------------------------------- MOVIES --------------------------------- */
+
+// ___________ MOVIE SETUP
+  function movieIdPath(movie) {
+    movie.idPath = "#/movies/detail/?id=" + movie.id;
+  }
+
+  function backupMovieImages(movie) {
+    if(movie.poster_path === null) {
+      movie.imgPath = "../../imgs/movie-backup.png";
+    }
+    else {
+      movie.imgPath = "http://image.tmdb.org/t/p/w185" + movie.poster_path;
+    }
+  }
+
+// __________ MOVIE RETURN
+  this.getRecentMovies = function() {
+    return jsonPad.getData( movieApi.recentUrl(), movieApi.callback() ).then(
+      function successCallback(response) {
+        var lessThan = function(prop, val) {
+          return function(item) {
+            return item[prop] <= val;
+          }
+        }
+
+        var recentMovies = $filter("filter")(response.data.results, lessThan("release_date", localDate.getCurrentDate() ));
+        var mediaList = $filter("orderBy")(recentMovies, "release_date", reverse = true);
+
+        angular.forEach(mediaList, function(movie) {
+          movieIdPath(movie);
+          backupMovieImages(movie);
+        });
+
+        // console.log(mediaList);
+        return mediaList;
+    });
+  }
+
+// ---
+
+  this.getUpcomingMovies = function() {
+    return jsonPad.getData( movieApi.upcomingUrl(), movieApi.callback() ).then(
+      function successCallback(response) {
+        var greaterThan = function(prop, val) {
+          return function(item) {
+            return item[prop] >= val;
+          }
+        }
+
+        var upcomingMovies = $filter("filter")(response.data.results, greaterThan("release_date", localDate.getTomorrowDate() ));
+        var mediaList = $filter("orderBy")(upcomingMovies, "release_date");
+
+        angular.forEach(mediaList, function(movie) {
+          movieIdPath(movie);
+          backupMovieImages(movie);
+        });
+
+        // console.log(mediaList);
+        return mediaList;
+    });
+  }
+
+
+/* ----------------------------------- TV ----------------------------------- */
+
+// ______________ TV SETUP
+  function checkOrigin(prop, val) {
+    return function(item) {
+      return item[prop].length === 0 || item[prop].toString() === val;
+    }
+  }
+
+  function tvIdPath(show) {
+    show.idPath = "#/tv/detail/?id=" + show.id;
+  }
+
+  function backupTvImages(show) {
+    if(show.poster_path === null) {
+      show.imgPath = "../../imgs/tv-backup.png";
+    }
+    else {
+      show.imgPath = "http://image.tmdb.org/t/p/w185" + show.poster_path;
+    }
+  }
+
+  function tvTitle(show) {
+    show.title = show.name;
+  }
+
+  function tvRelease(show) {
+    show.release_date = show.first_air_date;
+  }
+
+// _____________ TV RETURN
+  this.getRecentTv = function() {
+    return jsonPad.getData( tvApi.recentUrl(), tvApi.callback() ).then(
+      function successCallback(response) {
+        var mediaList = $filter("filter")( response.data.results, checkOrigin("origin_country", "US") );
+
+        angular.forEach(mediaList, function(show) {
+          tvIdPath(show);
+          backupTvImages(show);
+          tvTitle(show);
+          tvRelease(show);
+        });
+
+        // console.log(mediaList);
+        return mediaList;
+    });
+  }
+
+// ---
+
+  this.getUpcomingTv = function() {
+    return jsonPad.getData( tvApi.upcomingUrl(), tvApi.callback() ).then(
+      function successCallback(response) {
+        var upcomingTv = $filter("filter")( response.data.results, checkOrigin("origin_country", "US") );
+        var mediaList = $filter("orderBy")(upcomingTv, "first_air_date");
+
+        angular.forEach(mediaList, function(show) {
+          tvIdPath(show);
+          backupTvImages(show);
+          tvTitle(show);
+          tvRelease(show);
+        });
+
+        // console.log(mediaList);
+        return mediaList;
+    });
+  }
+
+
+/* ------------------------------ VIDEO GAMES ------------------------------ */
+
+// ____________ GAME SETUP
+  function getTrimmedArray(results, trimmedArray) {
+    for(i = 0; i < results.length; i++) {
+      if(i === 0) {
+        trimmedArray.push(results[i]);
+      }
+
+      else if(trimmedArray.length < 20) {
+        var hasDuplicate = false;
+
+        for(v = 0; v < trimmedArray.length; v++) {
+          if(results[i].game.id === trimmedArray[v].game.id) {
+            hasDuplicate = true;
+          }
+        }
+
+        if(hasDuplicate === false) {
+          trimmedArray.push(results[i]);
+        }
+      }
+    }
+    return trimmedArray;
+  }
+
+  function gameIdPath(game) {
+    game.idPath = "#/games/detail/?id=" + game.game.id;
+  }
+
+  function backupGameImages(game) {
+    if(game.image.small_url === "https://www.giantbomb.com/api/image/scale_small/3026329-gb_default-16_9.png") {
+      game.imgPath = "../../imgs/game-backup.png";
+    }
+    else {
+      game.imgPath = game.image.small_url;
+    }
+  }
+
+  function gameTitle(game) {
+    game.title = game.name;
+  }
+
+  function formatDate(game) {
+    game.release_date = new Date(game.expected_release_year + "/" + game.expected_release_month + "/" + game.expected_release_day);
+  }
+
+// ___________ GAME RETURN
+  this.getRecentGames = function() {
+    return jsonPad.getData( gameApi.recentUrl(), gameApi.callback() ).then(
+      function successCallback(response) {
+        var trimmedArray = [];
+        getTrimmedArray(response.data.results, trimmedArray);
+
+        var mediaList = $filter("filter")(trimmedArray, {release_date:""});
+
+        angular.forEach(mediaList, function(game) {
+          gameIdPath(game);
+          backupGameImages(game);
+          gameTitle(game);
+          game.release_date = game.release_date.replace(/ /g,"T");
+        });
+
+        // console.log(mediaList);
+        return mediaList;
+    });
+  }
+
+// ---
+
+  this.getUpcomingGames = function() {
+    return jsonPad.getData( gameApi.upcomingUrl(), gameApi.callback() ).then(
+      function successCallback(response) {
+        var trimmedArray = [];
+        getTrimmedArray(response.data.results, trimmedArray);
+
+        var mediaList = $filter("filter")(trimmedArray, {expected_release_year:"",
+          expected_release_month:"",
+          expected_release_day:""}
+        );
+
+        angular.forEach(mediaList, function(game) {
+          gameIdPath(game);
+          backupGameImages(game);
+          gameTitle(game);
+          formatDate(game);
+        });
+
+        // console.log(mediaList);
+        return mediaList;
+    });
+  }
+
+
+}]);
