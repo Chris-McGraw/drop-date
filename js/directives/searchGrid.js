9bff4022608ad272762318e04472c6a427878847
Chris McGraw <chris.rmcgraw1@gmail.com>
2020-02-03 18:41:20 -0500

    add getPageButtons ng-click functionality to searchGrid directive

--- js/directives/searchGrid.js
+++ js/directives/searchGrid.js
@@ -40,8 +40,86 @@ app.directive("searchGrid", ["jsonPad", "gameApi", "movieApi", "tvApi", "userSea
           });
         }
 
+
+
+
+
+        function getPageButtons(response) {
+          var pageCountTotal = 0;
+          scope.pageButtons = [];
+
+          scope.totalResults = response.data.number_of_total_results;
+          scope.pageResultFirst = 1;
+
+          if(response.data.number_of_total_results > 10) {
+            scope.pageResultLast = 10;
+          }
+          else {
+            scope.pageResultLast = response.data.number_of_total_results;
+          }
+
+          if(response.data.number_of_total_results % 10 === 0) {
+            pageCountTotal = (response.data.number_of_total_results / 10);
+          }
+          else {
+            pageCountTotal = ( Math.floor(response.data.number_of_total_results / 10) + 1 );
+          }
+
+          for(i = 0; i < pageCountTotal; i++) {
+            scope.pageButtons.push({number: i + 1});
+          }
+        }
+
+
+        function getCurrentPageResults(results, number) {
+          var currentPageResults = [];
+
+          number = number - 1;
+
+          for(i = (number * 10); i < results.length; i++) {
+            if(currentPageResults.length < ( (number + 1) * 10) ) {
+              currentPageResults.push(results[i]);
+            }
+          }
+
+          return currentPageResults;
+        }
+
+
+
+
+
         jsonPad.getData( gameApi.searchUrl(), gameApi.callback() ).then(
           function successCallback(response) {
+
+            getPageButtons(response);
+
+            scope.clickTest = function(number, $index) {
+            // get current page results from API response
+              scope.results = getCurrentPageResults(response.data.results, number);
+
+            // get the number of the first result on the current page
+              scope.pageResultFirst = ((number * 10) - 9);
+
+            // get the number of the last result on the current page
+              if(response.data.number_of_total_results > (number * 10)) {
+                scope.pageResultLast = (number * 10);
+              }
+              else {
+                scope.pageResultLast = response.data.number_of_total_results;
+              }
+
+            // remove "selected class" styling from all result page buttons
+            // add "selected class" styling to selected result page button
+              for(i = 0; i < document.getElementsByClassName("result-page-btn").length; i++) {
+                document.getElementsByClassName("result-page-btn")[i].style.backgroundColor = "white";
+              }
+              document.getElementsByClassName("result-page-btn")[$index].style.backgroundColor = "red";
+
+            // scroll to window top
+              window.scrollTo(0, 0);
+            }
+
             scope.results = response.data.results;
 
             resultLink();
