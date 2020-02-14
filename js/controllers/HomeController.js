974e2b2add1882104ba6f256c8055aa901eccc0c
Chris McGraw <chris.rmcgraw1@gmail.com>
2020-02-14 09:47:09 -0500

    add media-home-hero-containers to media home views

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,3 +1,11 @@
-app.controller("HomeController", ["$scope", function($scope) {
+app.controller("HomeController", ["$scope", "fillMediaRow", function($scope, fillMediaRow) {
   $scope.title = "Current View : Home";
+
+
+// ___________ GAME RETURN
+  // fillMediaRow.getRecentGames().then(function(data) {
+  //   console.log(data);
+  // });
+
+
 }]);
