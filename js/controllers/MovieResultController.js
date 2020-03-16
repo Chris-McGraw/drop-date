eb43edb9fd7d850f497a8ab2953f23a60952a15c
Chris McGraw <chris.rmcgraw1@gmail.com>
2020-03-15 20:03:29 -0400

    edit all mediaResult controllers/views(add media search forms)

--- js/controllers/MovieResultController.js
+++ js/controllers/MovieResultController.js
@@ -1,3 +1,15 @@
-app.controller("MovieResultController", ["$scope", "$location", function($scope, $location) {
+app.controller("MovieResultController", ["$scope", "$location", "userSearch", function($scope, $location, userSearch) {
   $scope.searchQuery = $location.search().search;
+
+  $scope.getInput = function() {
+    var input = $scope.inputVal;
+    userSearch.setData(input);
+
+    $location.url($location.path());
+
+    $location.search("search", userSearch.getData());
+    $location.path("/movies/results/");
+  }
+
+
 }]);
