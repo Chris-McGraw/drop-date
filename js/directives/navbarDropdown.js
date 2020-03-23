77b6b96b56be54f6049baa20a99bbba9d9decf76
Chris McGraw <chris.rmcgraw1@gmail.com>
2020-03-23 19:13:40 -0400

    create navbarDropdown directive(...)
    combine dropdownNavbar & dropdownSearch directives

--- js/directives/navbarDropdown.js
+++ js/directives/navbarDropdown.js
@@ -1,21 +1,85 @@
-app.directive("dropdownSearch", ["$location", "userSearch", function($location, userSearch) {
+app.directive("navbarDropdown", ["$location", "userSearch", function($location, userSearch) {
   return {
     link: function(scope, element, attrs) {
 // _____________ VARIABLES
+      var dropdownNavActive = true;
+
+      var hamburgerBarTop = document.getElementById("hamburger-bar-top");
+      var hamburgerBarMiddle = document.getElementById("hamburger-bar-middle");
+      var hamburgerBarBottom = document.getElementById("hamburger-bar-bottom");
+
+      var dropdownNav = document.getElementById("dropdown-nav");
+      var dropdownLink = document.getElementsByClassName("dropdown-link");
+      var gameViewDropdownLink = document.getElementById("game-view-dropdown-link");
+      var movieViewDropdownLink = document.getElementById("movie-view-dropdown-link");
+      var tvViewDropdownLink = document.getElementById("tv-view-dropdown-link");
+
+// ---
+
       var dropdownSearchActive = false;
 
       var searchToggleIconOpen = document.getElementsByClassName("search-toggle-icon-open");
       var searchToggleIconClose = document.getElementsByClassName("search-toggle-icon-close");
 
       var dropdownSearchContainer = document.getElementById("dropdown-search-container");
+      var dropdownSearchSubmit = document.getElementById("dropdown-search-submit");
       var dropdownSearchInput = document.getElementById("dropdown-search-input");
       var mediaSearchSelect = document.getElementById("media-search-select");
 
 
 // _____________ FUNCTIONS
+      function expandDropdownNav() {
+        hamburgerBarTop.classList.add("rotate-bar-top");
+        hamburgerBarMiddle.classList.add("hide-bar-middle");
+        hamburgerBarBottom.classList.add("rotate-bar-bottom");
+
+        dropdownNav.classList.add("dropdown-nav-expand");
+
+        angular.forEach(dropdownLink, function(link) {
+          link.tabIndex = 0;
+        });
+      }
+
+
+      function retractDropdownNav() {
+        hamburgerBarTop.classList.remove("rotate-bar-top");
+        hamburgerBarMiddle.classList.remove("hide-bar-middle");
+        hamburgerBarBottom.classList.remove("rotate-bar-bottom");
+
+        dropdownNav.classList.remove("dropdown-nav-expand");
+
+        angular.forEach(dropdownLink, function(link) {
+          link.tabIndex = -1;
+        });
+      }
+
+
+      scope.toggleDropdownNav = function() {
+        if(dropdownNavActive === true) {
+          retractDropdownNav();
+
+          dropdownNavActive = false;
+        }
+        else if(dropdownNavActive === false) {
+          expandDropdownNav();
+
+          dropdownNavActive = true;
+        }
+      }
+
+
+      function clearMediaViewType() {
+        gameViewDropdownLink.style.borderBottom = "none";
+        movieViewDropdownLink.style.borderBottom = "none";
+        tvViewDropdownLink.style.borderBottom = "none";
+      }
+
+// ---
+
       function expandDropdownSearch() {
         dropdownSearchContainer.classList.add("dropdown-nav-expand");
 
+        dropdownSearchSubmit.tabIndex = 0;
         dropdownSearchInput.tabIndex = 0;
         mediaSearchSelect.tabIndex = 0;
 
@@ -36,6 +100,7 @@ app.directive("dropdownSearch", ["$location", "userSearch", function($location, 
       function retractDropdownSearch() {
         dropdownSearchContainer.classList.remove("dropdown-nav-expand");
 
+        dropdownSearchSubmit.tabIndex = -1;
         dropdownSearchInput.tabIndex = -1;
         mediaSearchSelect.tabIndex = -1;
 
@@ -90,6 +155,35 @@ app.directive("dropdownSearch", ["$location", "userSearch", function($location, 
 
 
 // ________ EVENT HANDLERS
+      scope.$on("$locationChangeStart", function(event) {
+        if($location.path() === "/games/"
+        || $location.path() === "/games/detail/"
+        || $location.path() === "/games/results/") {
+          clearMediaViewType();
+          gameViewDropdownLink.style.borderBottom = "2px solid red";
+        }
+
+        else if($location.path() === "/movies/"
+        || $location.path() === "/movies/detail/"
+        || $location.path() === "/movies/results/") {
+          clearMediaViewType();
+          movieViewDropdownLink.style.borderBottom = "2px solid red";
+        }
+
+        else if($location.path() === "/tv/"
+        || $location.path() === "/tv/detail/"
+        || $location.path() === "/tv/results/") {
+          clearMediaViewType();
+          tvViewDropdownLink.style.borderBottom = "2px solid red";
+        }
+
+        else {
+          clearMediaViewType();
+        }
+      });
+
+// ---
+
       mediaSearchSelect.onchange = function() {
         if(mediaSearchSelect.value === "games") {
           dropdownSearchInput.placeholder = "enter a video game title...";
