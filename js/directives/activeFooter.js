55f6ba0a5d4f4ae1596cb65e56fd1d5a08406c6a
Chris McGraw <chris.rmcgraw1@gmail.com>
2020-04-06 19:29:13 -0400

    edit style(add activeFooter directive to footer)

--- js/directives/activeFooter.js
+++ js/directives/activeFooter.js
@@ -1,25 +1,14 @@
 app.directive("activeFooter", ["$route", function($route) {
   return {
     link: function(scope, element, attrs) {
-// _____________ VARIABLES
-      // var attrLink = document.getElementsByClassName("attr-link");
-
 
 // _____________ FUNCTIONS
-      // element[0].onmouseenter = function() {
-      //   this.classList.add("test-style");
-      // };
-      //
-      // element[0].onmouseleave = function() {
-      //   this.classList.remove("test-style");
-      // };
-
       element[0].ontouchstart = function() {
-        this.classList.add("test-style");
+        this.classList.add("attr-link-highlight");
       };
 
       element[0].ontouchend = function() {
-        this.classList.remove("test-style");
+        this.classList.remove("attr-link-highlight");
       };
 
 
