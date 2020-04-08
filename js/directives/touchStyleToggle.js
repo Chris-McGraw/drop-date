01e4f78ad7c750d5d4d04b12bec91b77701dbd73
Chris McGraw <chris.rmcgraw1@gmail.com>
2020-04-08 16:00:50 -0400

    edit touchStyleToggle directive(...)
    add to navbar page links

---
+++ js/directives/touchStyleToggle.js
@@ -0,0 +1,28 @@
+app.directive("touchStyleToggle", ["$route", function($route) {
+  return {
+    link: function(scope, element, attrs) {
+
+// ________ EVENT HANDLERS
+      element[0].ontouchstart = function() {
+        if( this.classList.contains("navbar-page-link") ) {
+          this.classList.add("navbar-link-highlight");
+        }
+        else {
+          this.classList.add("attr-link-highlight");
+        }
+      }
+
+
+      element[0].ontouchend = function() {
+        if( this.classList.contains("navbar-page-link") ) {
+          this.classList.remove("navbar-link-highlight");
+        }
+        else {
+          this.classList.remove("attr-link-highlight");
+        }
+      }
+
+
+    }
+  }
+}]);
