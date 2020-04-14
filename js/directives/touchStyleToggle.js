7b06969a67a3e24bbe0a26be5fa17d42a2a1bdcf
Chris McGraw <chris.rmcgraw1@gmail.com>
2020-04-14 12:24:18 -0400

    edit touchStyleToggle directive(...)
    clean up code

--- js/directives/touchStyleToggle.js
+++ js/directives/touchStyleToggle.js
@@ -4,11 +4,12 @@ app.directive("touchStyleToggle", ["$route", function($route) {
 
 // ________ EVENT HANDLERS
       element[0].ontouchstart = function() {
+      // navbar dropdown link
         if(this.children.length === 1
         && this.children[0].classList.contains("dropdown-link-text") ) {
           this.children[0].classList.add("navbar-link-highlight");
         }
-
+      // navbar dropdown search toggle button
         else if( this.children.length === 2
         && this.children[0].classList.contains("search-toggle-icon-open")
         && this.children[1].classList.contains("search-toggle-icon-close") ) {
@@ -16,51 +17,54 @@ app.directive("touchStyleToggle", ["$route", function($route) {
           this.children[1].children[0].classList.add("hamburger-highlight");
           this.children[1].children[2].classList.add("hamburger-highlight");
         }
-
+      // navbar logo
         else if(this.id === "drop-date-logo") {
           this.classList.add("logo-highlight");
         }
-
+      // navbar country select
         else if(this.id === "country-select") {
           this.classList.add("country-select-highlight");
         }
-
+      // navbar dropdown navigation toggle button
         else if(this.children.length === 3
         && this.id === "hamburger-menu") {
           this.children[0].classList.add("hamburger-highlight");
           this.children[1].classList.add("hamburger-highlight");
           this.children[2].classList.add("hamburger-highlight");
         }
-
+      // navbar link
+      // media row title link
         else if( this.classList.contains("navbar-page-link")
         || this.classList.contains("media-row-link") ) {
           this.classList.add("navbar-link-highlight");
         }
-
+      // feature hero image
         else if(this.children.length === 1
         && this.children[0].classList.contains("hero-media-img") ) {
           this.children[0].classList.add("carousel-tile-highlight");
         }
-
+      // feature hero media name
+      // feature hero media type
         else if( this.classList.contains("hero-media-name-link")
         || this.classList.contains("hero-media-type-link") ) {
           this.classList.add("navbar-link-highlight");
         }
-
+      // carousel tile
         else if( this.classList.contains("carousel-tile") ) {
           this.classList.add("carousel-tile-highlight");
         }
-
+      // feature hero scroll button
+      // carousel tile scroll button
         else if( this.classList.contains("hero-scroll")
         || this.classList.contains("carousel-scroll") ) {
           this.classList.add("scroll-button-highlight");
         }
-
+      // footer social link icon
         else if(this.children.length === 1
         && this.children[0].classList.contains("footer-link-icon") ) {
           this.children[0].classList.add("navbar-link-highlight");
         }
-
+      // footer attribution link
         else {
           this.classList.add("attr-link-highlight");
         }
@@ -68,11 +72,12 @@ app.directive("touchStyleToggle", ["$route", function($route) {
 
 
       element[0].ontouchend = function() {
+      // navbar dropdown link
         if(this.children.length === 1
         && this.children[0].classList.contains("dropdown-link-text") ) {
           this.children[0].classList.remove("navbar-link-highlight");
         }
-
+      // navbar dropdown search toggle button
         else if( this.children.length === 2
         && this.children[0].classList.contains("search-toggle-icon-open")
         && this.children[1].classList.contains("search-toggle-icon-close") ) {
@@ -80,51 +85,54 @@ app.directive("touchStyleToggle", ["$route", function($route) {
           this.children[1].children[0].classList.remove("hamburger-highlight");
           this.children[1].children[2].classList.remove("hamburger-highlight");
         }
-
+      // navbar logo
         else if(this.id === "drop-date-logo") {
           this.classList.remove("logo-highlight");
         }
-
+      // navbar country select
         else if(this.id === "country-select") {
           this.classList.remove("country-select-highlight");
         }
-
+      // navbar dropdown navigation toggle button
         else if(this.children.length === 3
         && this.id === "hamburger-menu") {
           this.children[0].classList.remove("hamburger-highlight");
           this.children[1].classList.remove("hamburger-highlight");
           this.children[2].classList.remove("hamburger-highlight");
         }
-
+      // navbar link
+      // media row title link
         else if( this.classList.contains("navbar-page-link")
         || this.classList.contains("media-row-link") ) {
           this.classList.remove("navbar-link-highlight");
         }
-
+      // feature hero image
         else if(this.children.length === 1
         && this.children[0].classList.contains("hero-media-img") ) {
           this.children[0].classList.remove("carousel-tile-highlight");
         }
-
+      // feature hero media name
+      // feature hero media type
         else if( this.classList.contains("hero-media-name-link")
         || this.classList.contains("hero-media-type-link") ) {
           this.classList.remove("navbar-link-highlight");
         }
-
+      // carousel tile
         else if( this.classList.contains("carousel-tile") ) {
           this.classList.remove("carousel-tile-highlight");
         }
-
+      // feature hero scroll button
+      // carousel tile scroll button
         else if( this.classList.contains("hero-scroll")
         || this.classList.contains("carousel-scroll") ) {
           this.classList.remove("scroll-button-highlight");
         }
-
+      // footer social link icon
         else if(this.children.length === 1
         && this.children[0].classList.contains("footer-link-icon") ) {
           this.children[0].classList.remove("navbar-link-highlight");
         }
-
+      // footer attribution link
         else {
           this.classList.remove("attr-link-highlight");
         }
